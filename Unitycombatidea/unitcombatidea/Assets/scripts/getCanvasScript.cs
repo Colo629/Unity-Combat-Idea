c1c48d38f6cdcbd4f6348302a95853a9d2c5ea4c
Colo629 <ethanhapp@Hotmail.com>
2020-02-19 23:27:12 -0700

    alch added

---
+++ Unitycombatidea/unitcombatidea/Assets/scripts/getCanvasScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getCanvasScript : MonoBehaviour
+{
+    public GameObject canvas;
+    private saveImage saveImage;
+    private CamInfo camInfo;
+    // Start is called before the first frame update
+    void awake()
+        {
+         saveImage = canvas.GetComponent<saveImage>();
+         camInfo = canvas.GetComponent<CamInfo>();
+        }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
