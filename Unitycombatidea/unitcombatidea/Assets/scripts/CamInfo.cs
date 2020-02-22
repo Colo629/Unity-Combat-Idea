2fa2284d07e5b605e087b58a8d8788e42fa157b7
Colo629 <ethanhapp@Hotmail.com>
2020-02-21 22:15:04 -0700

    deleted stuff

--- Unitycombatidea/unitcombatidea/Assets/scripts/CamInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CamInfo : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public Camera cam;
-}
