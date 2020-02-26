1d30b518e9872798018ad74954695932120b824b
colo6299 <rocketblox@gmail.com>
2020-02-25 17:10:43 -0800

    Add smooth locomotion

---
+++ unitcombatidea/Assets/scripts/SmoothMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SmoothMove : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 moveAxis;
+    public SteamVR_Action_Boolean lockAxis;
+
+    void Update()
+    {
+        if (lockAxis.GetActive(SteamVR_Input_Sources.LeftHand))
+        {
+            Vector3 playerPos = transform.position;
+            Vector3 contCoords = moveAxis.GetAxis(SteamVR_Input_Sources.LeftHand);
+            contCoords = new Vector3(contCoords.x, 0, contCoords.y);
+            playerPos = playerPos + transform.TransformPoint(contCoords * Time.deltaTime);
+        }
+        else if (lockAxis.GetActive(SteamVR_Input_Sources.RightHand))
+        {
+            Vector3 playerPos = transform.position;
+            Vector3 contCoords = moveAxis.GetAxis(SteamVR_Input_Sources.RightHand);
+            contCoords = new Vector3(contCoords.x, 0, contCoords.y);
+            playerPos = playerPos + transform.TransformPoint(contCoords * Time.deltaTime);
+        }
+    }
+}
