cc2e7a39984848dc5d1e54d2b8cc14ea75e6e459
colo6299 <rocketblox@gmail.com>
2020-02-25 17:16:33 -0800

    Update smooth locomotion

--- unitcombatidea/Assets/scripts/SmoothMove.cs
+++ unitcombatidea/Assets/scripts/SmoothMove.cs
@@ -7,19 +7,20 @@ public class SmoothMove : MonoBehaviour
 {
     public SteamVR_Action_Vector2 moveAxis;
     public SteamVR_Action_Boolean lockAxis;
+    public GameObject playerBody;
 
     void Update()
     {
         if (lockAxis.GetActive(SteamVR_Input_Sources.LeftHand))
         {
-            Vector3 playerPos = transform.position;
+            Vector3 playerPos = playerBody.transform.position;
             Vector3 contCoords = moveAxis.GetAxis(SteamVR_Input_Sources.LeftHand);
             contCoords = new Vector3(contCoords.x, 0, contCoords.y);
             playerPos = playerPos + transform.TransformPoint(contCoords * Time.deltaTime);
         }
         else if (lockAxis.GetActive(SteamVR_Input_Sources.RightHand))
         {
-            Vector3 playerPos = transform.position;
+            Vector3 playerPos = playerBody.transform.position;
             Vector3 contCoords = moveAxis.GetAxis(SteamVR_Input_Sources.RightHand);
             contCoords = new Vector3(contCoords.x, 0, contCoords.y);
             playerPos = playerPos + transform.TransformPoint(contCoords * Time.deltaTime);
